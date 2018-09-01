21ac0a20a775528d8cffefee052975bd031f25ca
Arek <t8534@o2.pl>
2018-09-01 16:53:51 +0200

    [master] init

---
+++ core_atmega/main.c
@@ -0,0 +1,18 @@
+/*
+ * core_atmega.c
+ *
+ * Created: 2018-09-01 09:37:13
+ * Author : arek
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
